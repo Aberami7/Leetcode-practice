12cf527b45de1e387a807ad07f7838e01f87f99a
Aberami K.A <abepadmanaban@gmail.com>
2026-08-20 15:09:49 +0530

    Time: 1 ms (30.22%) | Memory: 42.7 MB (77.66%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int a=0;
+        int l=0;
+        for(int i=0;i<s.length();i++)
+        {
+           if(s.charAt(i)=='A')
+           {
+            a++;
+            l=0;
+           }
+           else if(s.charAt(i)=='L')
+           {
+            l++;
+           if(l>=3)
+           {
+            return false;
+           }
+           }
+           else
+           {
+            l=0;
+           }
+           
+        
+        }
+        if(a>=2)
+        {
+            return false;
+        }
+        return true;
+}
+}
