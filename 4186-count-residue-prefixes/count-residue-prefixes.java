a38a74f0f78b599746dfd42cec0039e8fde88c95
Aberami K.A <abepadmanaban@gmail.com>
2026-08-20 14:36:15 +0530

    Time: 2 ms (73.25%) | Memory: 43.9 MB (68.31%) - LeetSync

---
+++ 4186-count-residue-prefixes/count-residue-prefixes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int residuePrefixes(String s) {
+        int count=0;
+       Set<Character> set=new HashSet<>();
+        for(int i=0;i<s.length();i++)
+        {
+            set.add(s.charAt(i));
+            if(set.size()==(i+1)%3)
+            {
+                count++;
+            }
+
+
+         
+        }
+        return count;
+    }
+}
